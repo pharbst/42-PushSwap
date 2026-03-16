ce04c3ab521fe824a3fc3283a493768457be570e
Peter Harbst <pharbst@3-G-9.42heilbronn.de>
2026-03-17 00:20:53 +0100

    fixed:     +- reducer     white space inf loop     error when not "\f\n\r\t\v 0123456789+-"     passed wrong argv index to ft_str_concat changes:     parse_error has now void *pointer_to_free and void **pointers_to_free     to free just one pointer and or an array of pointers     array must be NULL pointer terminated added:     documentation comments     doc string comments     comments

--- src/parsing/arg_parser.c
+++ src/parsing/arg_parser.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 13:54:38 by pharbst           #+#    #+#             */
-/*   Updated: 2026/03/14 19:45:06 by pharbst          ###   ########.fr       */
+/*   Updated: 2026/03/17 00:20:29 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,22 @@
 # include <stdbool.h>
 # include <string.h>
 
-// Small function to to print error, free a set of pointers, and the return value.
+// Small function to to print error, free a pointer, and/or a set of pointers, and the return value.
+// set 2nd and 3rd argument to NULL if not needed.
 // Usage when returning pointer value
 // return (parse_error("Error\n", pointer_to_free, NULL))	// returns NULL
 // Usage when returning non pointer value
 // return (*parse_error("Error\n", pointer_to_free, &5))	// returns 5
-void *parse_error(const char *error_msg, void **pointer_to_free, void *return_value) {
+void *parse_error(const char *error_msg, void *pointer_to_free, void **pointers_to_free, void *return_value) {
 	unsigned int	index;
 
 	write(1, error_msg, strlen(error_msg));
 
 	index = 0;
-	while (pointer_to_free && pointer_to_free[index])
-		free(pointer_to_free[index++]);
+	if (pointer_to_free)
+		free(pointer_to_free);
+	while (pointers_to_free && pointers_to_free[index])
+		free(pointers_to_free[index++]);
 	return (return_value);
 }
 
@@ -54,7 +57,8 @@ char *ft_str_concat(char **to_concat, char *seperator) {
 	if (!to_concat)
 		return ((char*)to_concat);
 	offset = 0;
-	cached1, result = strdup(to_concat[offset++]);
+	result = strdup(to_concat[offset++]);
+	cached1 = result;
 	while (to_concat[offset]) {
 		cached2 = strdup(to_concat[offset++]);
 		result = strjoin(cached1, seperator);
@@ -80,48 +84,60 @@ char *normalize_and_reduce(char *input) {
 	if (!input)
 		return (input);
 	index = 0;
+	printf("input: %s", input);
+	// exit(0);
 
-	while (input[index]) {
+	while (strchr("\f\n\r\t\v 0123456789+-", input[index]) && index <= strlen(input)) {
+		printf("%zu\n", index);
+		// skip digits
+		while (strchr("0123456789", input[index]))
+			index++;
+		// switch tabs etc to white space
 		if (strchr("\t\n\r\f\v", input[index]))
 			input[index] = ' ';
 		// seems incorrect
+		// yes it is this overwrites the actual whitespace i want to keep
+		// solution: memmove dest must be +1 and src +2
+		// this works cause the \0 is protecting strlen and we already checked the index + 1 is some char belonging to the str so there must be at least one more byte (index + 2)
 		if (input[index] == ' ' && strchr("\f\n\r\t\v ", input[index + 1])) {
-			memmove(&input[index], &input[index + 1], strlen(&input[index + 1]) + 1);
+			memmove(&input[index + 1], &input[index + 2], strlen(&input[index + 2]) + 1);
 			continue;
 		}
+		// 0 vars to count number - signs
 		neg = 0;
 		i = 0;
+		// count - signs to reduce
 		while (strchr("+-", input[index + i]))
 			if (input[index + i++] == '-')
 				neg++;
-		if (i) {
+		// when i != 0 means at least one sign was counted
+		// setting the condition to i > 1 should also work since with just one sign there is nothing to reduce
+		if (i > 1) {
 			if (neg % 2)
 				input[index] = '-';
 			else
 				input[index] = '+';
 			memmove(&input[index + 1], &input[index + i], strlen(&input[index + i]) + 1);
 			if (!strchr("0123456789", input[(index++) + 1]))
-				return (parse_error("Error\n", (void**)&input, NULL));
+				return (parse_error("Error1\n", (void*)input, NULL, NULL));
 		}
-		while (strchr("0123456789", input[index]))
+		if (input[index] == ' ')
 			index++;
-		if (!strchr("\f\n\r\t\v ", input[index]))
-			return (parse_error("Error\n", (void**)&input, NULL));
+		// if (!strchr("\f\n\r\t\v ", input[index]))
+		// 	return (parse_error("Error2\n", (void*)input, NULL, NULL));
 	}
+	if (input[index] && !strchr("\f\n\r\t\v 0123456789+-", input[index]))
+		return (parse_error("Error\n", (void*)input, NULL, NULL));
 	result = strdup(input);
 	free(input);
 	return (result);
 }
 
-// char *input_checker_reducer(char *normalized_input) {
-	
-// }
-
 // Wrapper function creates a normalized string from argv
 char *normalize_input(char **argv) {
 	char	*normalized_input;
 
-	normalized_input = normalize_and_reduce(ft_str_concat(argv, " "));
+	normalized_input = normalize_and_reduce(ft_str_concat(&argv[1], " "));
 	return (normalized_input);
 }
 
@@ -140,14 +156,14 @@ int count_spaces(char *input) {
 }
 
 // creates an integer array of the normalized string
-int *create_raw_stack(char *input_string, int num_of_elements) {
-	int		*raw_stack;
+// int *create_raw_stack(char *input_string, int num_of_elements) {
+// 	int		*raw_stack;
 
-	raw_stack = calloc(num_of_elements, sizeof(int));
-	if (!raw_stack)
-		return (raw_stack);
+// 	raw_stack = calloc(num_of_elements, sizeof(int));
+// 	if (!raw_stack)
+// 		return (raw_stack);
 	
-}
+// }
 
 #include <stdio.h>
 
@@ -155,17 +171,18 @@ int main(int argc, char **argv) {
 	char	*normalized;
 	int		*array;
 
-	printf("result:|%s|\n", normalize_input(argv));
+	if (argc > 2)
+		printf("result:|%s|\n", normalize_input(argv));
 	return (0);
 }
 
 // argument parser creates the stack a from argv
 // t_stack *arg_parser(char **argv) {
-// 	char	*normal_input;
+// 	char	*normalized_input;
 // 	int		number_of_values;
 // 	int		*raw_stack;
 
-// 	normal_input = normalize_input(&argv[1]);	//&argv[1] == argv + 1
-// 	number_of_values = count_spaces(normal_input) + 1;
-// 	raw_stack = create_raw_stack(normal_input, number_of_values);
+// 	normalized_input = normalize_input(&argv[1]);	//&argv[1] == argv + 1
+// 	number_of_values = count_spaces(normalized_input) + 1;
+// 	raw_stack = create_raw_stack(normalized_input, number_of_values);
 // }
