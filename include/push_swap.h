b55fdc3a35d4bf17dce13b6c32648a7752bf763a
Peter Harbst <pharbst@3-G-9.42heilbronn.de>
2026-03-17 05:23:07 +0100

    added struct and function definitions

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 12:57:55 by pharbst           #+#    #+#             */
-/*   Updated: 2026/03/14 16:27:27 by pharbst          ###   ########.fr       */
+/*   Updated: 2026/03/17 05:03:16 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@
 # include <string.h>
 
 // Data Structs
+typedef struct s_int_stack {
+	size_t	len;
+	int		*raw_stack;
+}	t_int_stack;
+
 typedef struct s_node {
 	unsigned int	index;
 	unsigned int	chunk_id;
@@ -30,5 +35,18 @@ typedef struct s_node {
 typedef struct s_stack {
 	t_node	*stack;
 }	t_stack;
+
+// Function definitions
+char			*strjoin(char const *s1, char const *s2);
+char			*ft_str_concat(char **to_concat, char *seperator);
+char			*normalize_and_reduce(char *input);
+char			*normalize_input(char **argv);
+int				count_spaces(char *input);
+void			isolate_element(char *input_string, size_t *index, int *to_insert, size_t *sub_index);
+bool			insert_values(t_int_stack *int_stack, char *input_string);
+t_int_stack		*create_raw_stack(char *input_string, int num_of_elements);
+unsigned int	*ranker(t_int_stack *original);
+void			*parse_error(const char *error_msg, void *pointer_to_free, void **pointers_to_free, void *return_value);
+bool			_check_sorted(t_int_stack *int_stack);
 
 #endif
