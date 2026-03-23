e52c7698a730b5590bc0ca7440b99c3b4c2a846d
Peter Harbst <pharbst@3-G-9.42heilbronn.de>
2026-03-23 18:24:10 +0100

    most logic is done     execute_job and direct_sort functions are missing

--- src/algorythm/midpoint.c
+++ src/algorythm/midpoint.c
@@ -11,9 +11,9 @@
 // use "job queue" and create 2 jobs at once first job is the one with the larger chunk second is on top -> resulting in: top job push b and second top job (job with larger chunk) push b + rotate 
 // this gives room for a lot of operation saves because stack a gets rotated a lot and the rotate can then be done using rr rotate both stacks at once
 
-void partition(t_chunk chunk, t_stack *stack_a, t_stack *stack_b) {
+// void partition(t_chunk chunk, t_stack *stack_a, t_stack *stack_b) {
 
-}
+// }
 
 // chunk initial state:
 // chunk.min = 1
@@ -21,35 +21,89 @@ void partition(t_chunk chunk, t_stack *stack_a, t_stack *stack_b) {
 // chunk.pivot = (chunk.min + chunk.max) \ 2
 
 // concrete example:
-// chunk.min = 1
+// chunk.min = 0
 // chunk.max = 500
-// chunk.pivot = 250.5 // 250 is enough
+// chunk.pivot = 250
 // chunk.lives_on = STACK_A
 
 // creates a subchunk from a to b with the lower 250 elements
 // create a chunk struct with old pivot + (old pivot /2) = 375 lives on STACK_A
 // call sort with that chunk struct
 // create a chunk struct with old pivot / 2 = 125 lives on STACK_B
-// 
 
-t_int_stack *sort(t_chunk chunk, t_stack *stack_a, t_stack *stack_b) {
-	if (issorted(stack_a)) {
-		// if stack_a threshold or stack_a chunk threshold
-		if (stack_a->size < 4 /*|| stack_a top chunk element count < 3*/)
-			sort_a(stack_a);
-			// sort directly
-		// else
-		else
-			// partition(chunk data struct, stack_a, stack_b)
-			// create subchunk
-		sort(stack_a, stack_b);
-		// sort()
+// at start of sort create 2nd chunk struct to increase hit rate
+// put both chunk structs onto stack where 2nd chunk struct is top
+// this only until stack a has less than 4 elements and only during initial push phase
+// later pop them one by one and process them in "reverse order"
+
+
+t_chunk create_job(t_chunk old_job, bool lives_on) {
+	t_chunk	new_job;
+
+	if (lives_on == STACK_A) {
+		new_job.lives_on = lives_on;
+		new_job.min = old_job.pivot;
+		new_job.max = old_job.max;
+		new_job.pivot = (new_job.min + new_job.max) / 2;
 	}
-	else if (stack_b->size) {
-		// check top "chunk" threshold
-			// push / sort directly
-		// else
-			// create subchunk
-		// sort()
+	else if (lives_on == STACK_B) {
+		new_job.lives_on = lives_on;
+		new_job.min = old_job.min;
+		new_job.max = old_job.pivot;
+		new_job.pivot = (new_job.min + new_job.max) / 2;
+	}
+	return (new_job);
+}
+
+// just_read flag to true if you want to get just the pointer to the op list
+// if false to_join and op_list are joined and NULL is returned
+t_stack *join_op_list(unsigned int to_join, bool just_read) {
+	static t_stack	*op_list;
+	t_stack			*old_list;
+
+	if (just_read)
+		return (op_list);
+	if (!op_list)
+		op_list = calloc(1000, sizeof(unsigned int));
+	if (op_list->size == op_list->capacity) {
+		old_list = op_list;
+		op_list = calloc(old_list->capacity + 1000, sizeof(unsigned int));
+		free(old_list);
+	}
+	// join operation to stack
+	return (NULL);
+}
+
+void execute_job() {
+
+}
+
+void direct_sort() {
+
+}
+
+void sort(t_chunk job, t_stack *stack_a, t_stack *stack_b) {
+	if (job.lives_on == STACK_A) {
+		if (job.max - job.min > 1) { // is chunk larger than 2 elements
+			execute_job(job, stack_a, stack_b);
+			sort(create_job(job, STACK_A), stack_a, stack_b);
+			sort(create_job(job, STACK_B), stack_a, stack_b);
+		}
+		else
+			direct_sort(stack_a, stack_b);
+			// direct sort
+	}
+	else if (job.lives_on == STACK_B) {
+		if (job.max - job.min > 1) { // chunk is larger than 2 elements
+			// push above pivot
+			execute_job(job, stack_a, stack_b);
+			sort(create_job(job, STACK_A), stack_a, stack_b);
+			sort(create_job(job, STACK_B), stack_a, stack_b);
+		}
+		else {
+			// push chunk to a
+			execute_job(job, stack_a, stack_b);
+			sort(create_job(job, STACK_A), stack_a, stack_b);
+		}
 	}
 }
